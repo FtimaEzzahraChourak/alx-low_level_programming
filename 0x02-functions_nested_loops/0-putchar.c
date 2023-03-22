1771e8f61dcd9bbaaf3f7685dc3bb6644dceeded
fatima ezzahra chourak <chourakfatimaezzahra8@gmaile.com>
2023-03-21 17:01:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,9 @@
  * main - print putchar
  * Return: always 0
  */
-
 int main(void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar \n");
 
 	return (0);
 
