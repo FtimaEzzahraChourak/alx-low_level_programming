67fe696086ac93e544662606361eeaccae55a9e5
fatima ezzahra chourak <chourakfatimaezzahra8@gmaile.com>
2023-03-23 14:29:47 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
 /**
-* print_numbers - print numbers 0 - 9
-* return: void
-*/
+ * print_numbers - print 0-9
+ * return: void
+ */
 
 void print_numbers(void)
-
 {
+	char c;
 
-        int i;
-
-
-        for (i = 0; i <= 9; i++)
-
-        {
-
-                _putchar(i);
-
-        }
-
-        _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
