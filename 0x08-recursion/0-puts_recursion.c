50fe100ec6beaf77788e89fd27745ce4d2e195a0
fatima ezzahra chourak <chourakfatimaezzahra8@gmaile.com>
2023-04-05 13:21:29 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- *  function that prints a string, followed by a new line.
  * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success);
  */
 void _puts_recursion(char *s)
 {
