036a5b893778f7c9db28ddf3759f10e3be30a903
fatima ezzahra chourak <chourakfatimaezzahra8@gmaile.com>
2023-05-08 14:19:07 -0700

    task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    if (filename == NULL)
+        return 0;
+
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return 0;
+
+    char buffer[1024];
+    ssize_t total_read = 0;
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
+    {
+        if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
+        {
+            close(fd);
+            return 0;
+        }
+        total_read += bytes_read;
+        if (total_read >= letters)
+            break;
+    }
+
+    close(fd);
+    return total_read;
+}
+
